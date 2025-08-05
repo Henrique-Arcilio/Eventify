794f53995b8fdb2e62401f32562f767fa26c30c6
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-08-05 10:54:43 -0300

    feat: define event model and UF enum

---
+++ ms-event-manager/src/main/java/com/arcilio/henrique/ms_event_manager/domain/enums/Uf.java
@@ -0,0 +1,7 @@
+package com.arcilio.henrique.ms_event_manager.domain.enums;
+
+public enum Uf {
+    AC, AL, AP, AM, BA, CE, DF, ES, GO, MA,
+    MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN,
+    RS, RO, RR, SC, SP, SE, TO;
+}
---
+++ ms-event-manager/src/main/java/com/arcilio/henrique/ms_event_manager/domain/model/Event.java
@@ -0,0 +1,46 @@
+package com.arcilio.henrique.ms_event_manager.domain.model;
+
+import com.arcilio.henrique.ms_event_manager.domain.enums.Uf;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Document("events")
+@Getter @Setter @ToString
+public class Event {
+    @Id
+    private String id;
+    private String eventName;
+    private LocalDateTime localDateTime;
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private Uf uf;
+
+    public Event(String eventName, LocalDateTime localDateTime, String cep, String logradouro, String bairro, String cidade, Uf uf) {
+        this.eventName = eventName;
+        this.localDateTime = localDateTime;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Event event)) return false;
+        return Objects.equals(id, event.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}

08f545868337d147705e41f3d1efd4776b39c01c
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-08-06 12:45:10 -0300

    feat: add UserDetails(User) implementation and userRepository

---
+++ ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/domain/model/User.java
@@ -0,0 +1,76 @@
+package com.arcilio.henrique.ms_ticket_manager.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Document("users")
+@Getter @Setter
+@EqualsAndHashCode
+public class User  implements UserDetails, Serializable {
+    private static final long serialVersion = 1L;
+
+    public User() {}
+
+    @Id
+    private String id;
+
+    private String username;
+
+    private String fullname;
+
+    private String password;
+
+    private Boolean accountNonExpired;
+
+    private Boolean accountNonLocked;
+
+    private Boolean credentialsNonExpired;
+
+    private Boolean enabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+}

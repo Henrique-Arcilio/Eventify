77d5f8004aad069f70f6412d2b7022a4e4953f72
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-08-06 13:08:06 -0300

    feat: add ErrorMessage class, InvalidJwt exception and ExceptionHandler

---
+++ ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/application/exception/security/TicketExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.arcilio.henrique.ms_ticket_manager.application.exception.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TicketExceptionHandler {
+    @ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public ResponseEntity<ErrorMessage> invalidJwtAuthentication
+            (InvalidJwtAuthenticationException exception, HttpServletRequest request){
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ErrorMessage(request, HttpStatus.FORBIDDEN, exception.getMessage() ));
+    }
+}

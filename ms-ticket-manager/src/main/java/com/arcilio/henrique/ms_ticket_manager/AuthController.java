48ad35a37a0274dc9f31cb0320d8c38d49e1e8b1
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-08-06 17:25:13 -0300

    feat: add AuthController and it's methods
    - signIn method to authenticate using a existente user (receives a
      response with a JWT token)
    - createUser method allows a user to create a new account

---
+++ ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/AuthController.java
@@ -0,0 +1,31 @@
+package com.arcilio.henrique.ms_ticket_manager;
+
+import com.arcilio.henrique.ms_ticket_manager.application.AuthService;
+import com.arcilio.henrique.ms_ticket_manager.application.representation.AccountCredentialsDto;
+import com.arcilio.henrique.ms_ticket_manager.application.representation.TokenDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<TokenDto> signIn(@RequestBody AccountCredentialsDto credentials){
+        return authService.signIn(credentials);
+    }
+    @PostMapping("/create-user")
+    public ResponseEntity<Void> createUser(@RequestBody AccountCredentialsDto credentials){
+        authService.createUser(credentials);
+        return ResponseEntity.noContent().build();
+    }
+
+}

fc5171e2bcc62c381877959cb38c1eb104281648
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-08-06 13:39:58 -0300

    feat: add createAcessToken method logic

--- ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/security/jwt/JwtTokenProvider.java
+++ ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/security/jwt/JwtTokenProvider.java
@@ -1,14 +1,17 @@
 package com.arcilio.henrique.ms_ticket_manager.security.jwt;
 
 import com.arcilio.henrique.ms_ticket_manager.application.representation.TokenDto;
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -28,5 +31,24 @@ public class JwtTokenProvider {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
         algorithm = Algorithm.HMAC256(secretKey.getBytes());
     }
+
+    public TokenDto createAcessToken(String username, List<String> roles){
+        Date now = new Date();
+
+        Date expiration = new Date(now.getTime() + expirationTimeMilis);
+        String acessToken = getAcessToken(username, roles, now, expiration);
+        String refreshToken = getRefreshToken(username, roles, now);
+        return new TokenDto(username, true, now, expiration, acessToken, refreshToken);
+    }
+
+    private String getAcessToken(String username, List<String> roles, Date now, Date expiration) {
+      return null;
+    }
+    private String getRefreshToken(String username, List<String> roles, Date now) {
+        return null;
+    }
+
+
+
 
 }

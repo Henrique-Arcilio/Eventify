021aa064ae5283e8f24cfd53b2bfe053cdce81cd
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-08-09 10:45:30 -0300

    feat: connect event-manager to ticket-manager

---
+++ ms-event-manager/src/main/java/com/arcilio/henrique/ms_event_manager/infra/clients/ticket/TicketManagerClient.java
@@ -0,0 +1,17 @@
+package com.arcilio.henrique.ms_event_manager.infra.clients.ticket;
+
+import com.arcilio.henrique.ms_event_manager.application.representation.CheckTicketDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "${feign.ticket-manager.name}", url= "${feign.ticket-manager.url")
+public interface TicketManagerClient {
+    @GetMapping("/tickets/events/{eventId}")
+    List<CheckTicketDto> checkTicketsByEventId(@PathVariable String eventId);
+
+    @GetMapping("/customer-tickets/events/{eventId}")
+    List<CheckTicketDto> checkCustomerTicketsByEventId(@PathVariable String eventId);
+}

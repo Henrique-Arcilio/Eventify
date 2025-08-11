a6c11cb50fb76c513e9b749a116a62bd3d8581fd
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-08-10 23:19:03 -0300

    feat: add AuthController documentation

--- ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/application/auth/AuthController.java
+++ ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/application/auth/AuthController.java
@@ -1,8 +1,12 @@
 package com.arcilio.henrique.ms_ticket_manager.application.auth;
 
+import com.arcilio.henrique.ms_ticket_manager.application.docs.AuthResourceDocs;
 import com.arcilio.henrique.ms_ticket_manager.application.representation.account.AccountCredentialsDto;
 import com.arcilio.henrique.ms_ticket_manager.application.representation.account.SignInCredentialsDto;
 import com.arcilio.henrique.ms_ticket_manager.application.representation.account.TokenDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-public class AuthController {
+@Tag(name = "Authentication")
+public class AuthController implements AuthResourceDocs {
 
     private final AuthService authService;
 
     @PostMapping("/signin")
+    @Override
     public ResponseEntity<TokenDto> signIn(@Valid @RequestBody SignInCredentialsDto credentials){
         return authService.signIn(credentials);
     }
     @PostMapping("/create-user")
+    @Override
     public ResponseEntity<Void> createUser(@Valid @RequestBody AccountCredentialsDto credentials){
         authService.createUser(credentials);
         return ResponseEntity.noContent().build();

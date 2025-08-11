f43559f9906a9216db27792fdaa0cf5773ff42f5
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2025-08-10 22:34:19 -0300

    refactor: add logs to CustomerTicketResource

--- ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/application/ticket/customer/CustomerTicketResource.java
+++ ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/application/ticket/customer/CustomerTicketResource.java
@@ -9,16 +9,17 @@ import com.arcilio.henrique.ms_ticket_manager.application.representation.tickets
 import com.arcilio.henrique.ms_ticket_manager.domain.model.CustomerTicket;
 import com.arcilio.henrique.ms_ticket_manager.domain.model.Ticket;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/customer-tickets")
 @RequiredArgsConstructor
@@ -28,6 +29,7 @@ public class CustomerTicketResource implements CustomerTicketResourceDocs {
 
     @GetMapping("/events/{eventId}")
     public ResponseEntity<List<GetCustomerTicketsByEventDto>> getByEventId(@PathVariable String eventId){
+        log.info("CustomerTicketResource getByEventId method accessed");
         List<CustomerTicket> tickets = ticketService.findByEventId(eventId);
         List<GetCustomerTicketsByEventDto> ticketDtos = TicketMapper.listOfPurchasedDto(tickets);
         return ResponseEntity.ok().body(ticketDtos);
@@ -35,6 +37,7 @@ public class CustomerTicketResource implements CustomerTicketResourceDocs {
 
     @GetMapping("/{id}")
     public ResponseEntity<GetCustomerTicketByIdDto> getById(@PathVariable String id){
+        log.info("CustomerTicketResource getById method accessed");
         CustomerTicket ticket = ticketService.findById(id);
         GetCustomerTicketByIdDto ticketDto = TicketMapper.ticketForSaleDto(ticket);
         return ResponseEntity.ok(ticketDto);
@@ -42,24 +45,28 @@ public class CustomerTicketResource implements CustomerTicketResourceDocs {
 
     @GetMapping
     public ResponseEntity<PageableDto> findAll(Pageable pageable, @AuthenticationPrincipal UserDetails userDetails){
+        log.info("CustomerTicketResource findAll method accessed");
         Page<CustomerTicket> tickets = ticketService.findAll(pageable, userDetails);
         return ResponseEntity.ok(PageableMapper.toDto(tickets));
     }
 
     @PostMapping("/{id}")
     public ResponseEntity<CustomerTicket> buy(@PathVariable String id, @AuthenticationPrincipal UserDetails userDetails){
+        log.info("CustomerTicketResource buy method accessed");
         CustomerTicket newTicket =  ticketService.create(id, userDetails);
         return ResponseEntity.ok(newTicket);
     }
 
     @PatchMapping("{id}/cancel")
     public ResponseEntity<Void> cancel(@PathVariable String id, @AuthenticationPrincipal UserDetails userDetails){
+        log.info("CustomerTicketResource cancel method accessed");
         ticketService.cancelUserTicket(id, userDetails);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/sales")
     public ResponseEntity<PageableDto> getAllForSale(Pageable pageable){
+        log.info("CustomerTicketResource getAllForSale method accessed");
         Page<Ticket> tickets = ticketService.findAllForSale(pageable);
         return ResponseEntity.ok(PageableMapper.toDto(tickets));
     }

c5a5170eba46cca80efb7c0de8c31ad31d82f194
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2026-07-15 19:51:35 -0300

    chore: allow ticket manager health endpoint access

--- ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/config/SecurityConfig.java
+++ ms-ticket-manager/src/main/java/com/arcilio/henrique/ms_ticket_manager/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.GET, "/api/v1/tickets/*").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/tickets").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.POST,"/api/v1/tickets").hasRole("ADMIN")
+                                .requestMatchers("/actuator/health").permitAll()
                                 .requestMatchers("/api/v1/tickets/events/**").permitAll()
                                 .requestMatchers("/api/v1/customer-tickets/events/**").permitAll()
                                 .requestMatchers("/api/v1/customer-tickets/sales").permitAll()
